421d19f9ed53e2b0b8b33134e3b771f980402fb2
Andrii Vorona <snickone77@gmail.com>
2022-09-17 15:05:16 +0200

    express app

--- myApp.js
+++ myApp.js
@@ -1,7 +1,18 @@
-
+require('dotenv').config()
 let express = require('express');
 let app = express();
+app.get('/', function(req, res){
+  const absolutePath = __dirname + '/views/index.html';
+  res.sendFile(absolutePath)
+})
 
+app.get('/json', (req, res) => {
+  const message = 'Hello json';
+  const responseMessage = process.env.MESSAGE_STYLE == 'uppercase' ? message.toUpperCase() : message ; 
+  res.send({"message": responseMessage});
+})
+
+app.use('/public', express.static(__dirname + '/public'));
 
 
 
