94abe31d2a2999fe562603caf794e1fb58e6261b
Andrii Vorona <snickone77@gmail.com>
2022-09-17 13:48:20 +0200

    new project

--- myApp.js
+++ myApp.js
@@ -1,1 +1,40 @@
-console.log('Hello World');
+
+let express = require('express');
+let app = express();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ module.exports = app;
