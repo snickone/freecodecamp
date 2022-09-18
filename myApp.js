9c070316a0e7b32b3d08a9df9e363d4e7b97ec71
Andriy Vorona <andrii.vorona@shelf.io>
2022-09-18 08:35:01 +0000

    added new methods

--- myApp.js
+++ myApp.js
@@ -1,6 +1,17 @@
+let bodyParser = require("body-parser");
 require('dotenv').config()
 let express = require('express');
 let app = express();
+
+app.use('/public', express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`)
+  next()
+})
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/', function(req, res){
   const absolutePath = __dirname + '/views/index.html';
   res.sendFile(absolutePath)
@@ -12,8 +23,24 @@ app.get('/json', (req, res) => {
   res.send({"message": responseMessage});
 })
 
-app.use('/public', express.static(__dirname + '/public'));
+app.get('/now', (req, res, next) => {
+  req.time = new Date().toString();
+  next();
+}, (req, res) => {
+  res.send({time: req.time})
+})
 
+app.get('/:word/echo', (req,res) => {
+  res.send({echo: req.params.word})
+})
+
+app.route('/name').get((req, res) => {
+  console.log(req.query);
+  res.send({name: `${req.query.first} ${req.query.last}`})
+}).post((req, res) => {
+  console.log(req.body.last);
+  res.send({name: `${req.body.first} ${req.body.last}`})
+})
 
 
 
